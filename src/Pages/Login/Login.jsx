5c4d60e889f8a6388611fcae93214c16caacca6c
mafizul24h <mafizul24h@gmail.com>
2023-12-30 20:01:52 +0600

    Register & Login

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -1,11 +1,49 @@
-import React, { useState } from 'react';
+import React, { useContext, useState } from 'react';
 import logo from './../../assets/logos/Group1329.png'
 import { FaRegEye, FaRegEyeSlash } from 'react-icons/fa';
 import SocialLogin from '../../components/SocialLogin/SocialLogin';
 import { Link } from 'react-router-dom';
+import { AuthContext } from '../../Providers/AuthProvider';
 
 const Login = () => {
+    const {singInUser} = useContext(AuthContext);
     const [show, setShow] = useState(false);
+    const [passError, setPasError] = useState();
+
+    const handleLogin = (event) => {
+        event.preventDefault();
+        const email = event.target.email.value;
+        const password = event.target.password.value;
+
+        setPasError('')
+        if (!/(?=.*[A-Z])/.test(password)) {
+            setPasError('Please add at least one uppercase');
+            return;
+        } else if (!/(?=.*?[a-z])/.test(password)) {
+            setPasError('Please add at lest one lowercase');
+            return;
+        } else if (!/(?=.*?[0-9])/.test(password)) {
+            setPasError('Please add at lest one number');
+            return;
+        } else if (!/(?=.*?[!@#$%^&*])/.test(password)) {
+            setPasError('Please add at lest one !@#$%^&*')
+            return;
+        } else if (password.length < 6) {
+            setPasError('Please add password minimum 6 character')
+            return;
+        }else {
+            setPasError('')
+        }
+        console.log(email, password);
+        singInUser(email, password)
+        .then(result => {
+            const loggedUser = result.user;
+            console.log(loggedUser);
+        })
+        .catch(error => {
+            console.log(error);
+        })
+    }
 
     return (
         <div>
@@ -14,7 +52,7 @@ const Login = () => {
                     <img style={{ height: '40px' }} src={logo} alt="Logo" />
                     <h2>Please Login!</h2>
                 </div>
-                <form>
+                <form onSubmit={handleLogin}>
                     <div className="mb-3">
                         <label for="exampleInputEmail1" className="form-label fw-bold">Email</label>
                         <input type="email" name='email' className="form-control" placeholder='Your Email' />
@@ -28,6 +66,7 @@ const Login = () => {
                             }
                         </div>
                     </div>
+                    {passError && <p className='text-danger'>{passError}</p>}
                     <input className="btn btn-primary w-100" type="submit" value={'Login'} />
                 </form>
                 <SocialLogin />
--- src/Pages/Login/Register.jsx
+++ src/Pages/Login/Register.jsx
@@ -1,12 +1,22 @@
-import React, { useState } from 'react';
+import React, { useContext, useState } from 'react';
 import logo from './../../assets/logos/Group1329.png'
 import { FaRegEye, FaRegEyeSlash } from 'react-icons/fa';
 import SocialLogin from '../../components/SocialLogin/SocialLogin';
 import { Link } from 'react-router-dom';
 import { useForm } from 'react-hook-form';
+import { AuthContext } from '../../Providers/AuthProvider';
+
+const image_Hosting_Key = import.meta.env.VITE_IMAGE_API_TOKEN;
 
 const Register = () => {
+    const {createUser} = useContext(AuthContext);
     const [show, setShow] = useState(false);
+    const [passError, setPasError] = useState();
+    const [confirmError, setConfirmError] = useState();
+    const [matchError, setMatchError] = useState();
+    const [image, setImage] = useState();
+    const imageURL = `https://api.imgbb.com/1/upload?key=${image_Hosting_Key}`;
+
     const {
         register,
         handleSubmit,
@@ -14,15 +24,81 @@ const Register = () => {
         formState: { errors },
     } = useForm();
     const onSubmit = (data) => {
-        console.log(data)
+
+        setPasError('');
+        setConfirmError('');
+
+        if (!/(?=.*[A-Z])/.test(data.password)) {
+            setPasError('Please add at least one uppercase');
+            return;
+        } else if (!/(?=.*?[a-z])/.test(data.password)) {
+            setPasError('Please add at lest one lowercase');
+            return;
+        } else if (!/(?=.*?[0-9])/.test(data.password)) {
+            setPasError('Please add at lest one number');
+            return;
+        } else if (!/(?=.*?[!@#$%^&*])/.test(data.password)) {
+            setPasError('Please add at lest one !@#$%^&*')
+            return;
+        } else if (data.password.length < 6) {
+            setPasError('Please add password minimum 6 character')
+            return;
+        }
+
+        if (!/(?=.*[A-Z])/.test(data.confirm)) {
+            setConfirmError('Please add at least one uppercase');
+            return;
+        } else if (!/(?=.*?[a-z])/.test(data.confirm)) {
+            setConfirmError('Please add at lest one lowercase');
+            return;
+        } else if (!/(?=.*?[0-9])/.test(data.confirm)) {
+            setConfirmError('Please add at lest one number');
+            return;
+        } else if (!/(?=.*?[!@#$%^&*])/.test(data.confirm)) {
+            setConfirmError('Please add at lest one !@#$%^&*')
+            return;
+        } else if (data.confirm.length < 6) {
+            setConfirmError('Please add password minimum 6 character')
+            return;
+        }
+
+        setMatchError('');
+        if (data.password !== data.confirm) {
+            setMatchError('Passwrod and Confirm Passwrod not matched');
+            return;
+        }
+
+        const formData = new FormData();
+        formData.append('image', image);
+
+        fetch(imageURL, {
+            method: 'POST',
+            body: formData
+        })
+        .then(res => res.json())
+        .then(resImg => {
+            if(resImg.success) {
+                data.image = resImg.data.display_url;
+                console.log(data);
+
+                createUser(data.email, data.password)
+                .then(result => {
+                    const logedUser = result.user;
+                    console.log(logedUser);
+                })
+                .catch(error => {
+                    console.log(error);
+                })
+            }
+        })
     }
 
     return (
-        <div>
+        <div >
             <div className="col-md-8 col-lg-6 bg-white p-4 p-lg-5 rounded mx-md-auto my-5 mx-3">
                 <div className='d-flex justify-content-around mb-3'>
                     <img style={{ height: '40px' }} src={logo} alt="Logo" />
-                    <h2>Please Login!</h2>
+                    <h2>Please Register!</h2>
                 </div>
                 <form onSubmit={handleSubmit(onSubmit)}>
                     <div className="mb-3">
@@ -37,7 +113,7 @@ const Register = () => {
                     </div>
                     <div className="mb-3 fw-bold">
                         <label for="formFile" className="form-label">Upload Your Profile Photo</label>
-                        <input className="form-control" type="file" id="formFile" />
+                        <input className="form-control" type="file" onChange={(e) => setImage(e.target.files[0])} id="formFile" required />
                     </div>
                     <div className="mb-3 password-container position-relative">
                         <label for="exampleInputPassword1" className="form-label fw-bold">Password</label>
@@ -47,6 +123,7 @@ const Register = () => {
                                 !show ? <FaRegEyeSlash onClick={() => setShow(!show)} className='' /> : <FaRegEye onClick={() => setShow(!show)} />
                             }
                         </div>
+                        {passError && <p className='text-danger'>{passError}</p>}
                         {errors.password && <p className='text-danger'>Please entry your password</p>}
                     </div>
                     <div className="mb-3 password-container position-relative">
@@ -57,6 +134,8 @@ const Register = () => {
                                 !show ? <FaRegEyeSlash onClick={() => setShow(!show)} className='' /> : <FaRegEye onClick={() => setShow(!show)} />
                             }
                         </div>
+                        {confirmError && <p className='text-danger'>{confirmError}</p>}
+                        {matchError && <p className='text-danger'>{matchError}</p>}
                         {errors.confirm && <p className='text-danger'>Please entry your confirm password</p>}
                     </div>
                     <input className="btn btn-primary w-100" type="submit" value={'Register'} />
